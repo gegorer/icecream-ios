031196b347bc4e511de85d18785356f76ecb380d
Shao Hang Kao <denehs@gmail.com>
2013-11-12 20:48:43 +0800

    Add loading view controller

---
+++ icecream/icecream/LoadingViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoadingViewController.h
+//  icecream
+//
+//  Created by Shao Hang Kao on 2013/11/12.
+//  Copyright (c) 2013年 gegorer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadingViewController : UIViewController
+
+@end
---
+++ icecream/icecream/LoadingViewController.m
@@ -0,0 +1,42 @@
+//
+//  LoadingViewController.m
+//  icecream
+//
+//  Created by Shao Hang Kao on 2013/11/12.
+//  Copyright (c) 2013年 gegorer. All rights reserved.
+//
+
+#import "LoadingViewController.h"
+
+@interface LoadingViewController ()
+
+@end
+
+@implementation LoadingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [self performSegueWithIdentifier:@"loadCompleteSegue" sender:self];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
